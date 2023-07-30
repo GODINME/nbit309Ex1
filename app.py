cfcfbd7643fe95dfc239f0062d567f70ca525b22
PAUL OFFEI <poffei@st.ug.edu.gh>
2023-07-30 01:53:22 +0000

    started rendering a template

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask('__name__')
+app = Flask(__name__, template_folder="")
 
 @app.route('/')
 def app_started():
-    return "App Started...!!!"
+    return render_template('app.html')
+
+@app.route('/hello')
+def hello():
+    return "Hello Customers!!!"
